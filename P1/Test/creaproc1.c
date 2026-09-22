8637fe1ebbb05a169e570f3a727738246f002dcf
Jose Miguel Martinez Garcia <jmaringarica07@icloud.com>
2026-09-22 15:10:31 +0200

    Test creaproc1, hecho

---
+++ P1/Test/creaproc1.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+pid_t pid;
+pid = fork();
+
+switch (pid)
+{
+
+case -1:
+printf ("No he podido crear el proceso hijo \n");
+break;
+
+case 0:
+
+printf ("Soy el hijo, mi PID es %d y mi PPID es %d \n",
+getpid(), getppid());
+sleep (20);
+
+break;
+
+default:
+
+sleep (30);
+printf ("\n Soy el padre, mi PID es %d y el PID de mi hijo es %d \n",
+getpid(), pid);
+
+break;
+}
+
+printf ("Final de ejecución de %d \n", getpid());
+exit (0);
+}
